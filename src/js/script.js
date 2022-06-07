ec1602005a9d27faeaba3d522c5027578b46b200
oleglugo <95176506+oleglugo@users.noreply.github.com>
2022-06-07 14:48:34 +0300

    add validate form and mask

--- src/js/script.js
+++ src/js/script.js
@@ -43,5 +43,41 @@ $(document).ready(function(){
         });
       });
 
+      function valideForms(form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+
+        messages: {
+          name: {
+            required:"Пожалуйста введите свое имя",
+            minlength: jQuery.validator.format("Введите более {0} символов!")
+          },
+          phone: "Пожалуйста введите свой телефон",
+
+          email: {
+            required: "Пожалуйста введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+        }
+
+      });
+    };
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
   });
+
 
