61ac02d253b28c61a83f803ec187d9b0f723ce78
oleg <lugovskoy.oleg@gmail.com>
2022-06-19 19:54:09 +0300

    add mailer

--- src/js/script.js
+++ src/js/script.js
@@ -81,6 +81,31 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
-  });
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      if (!$(this).valid ()) {   /* скрипт валидации пустых полей */
+        return;
+      }
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #ty').fadeIn('slow');
+
+          $('form').trigger('reset');
+      });
+      return false;
+    });
+
+
+
+
+
+
+});
 
 
